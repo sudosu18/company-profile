6f001899523f10e4461f863f3ddadbab781053d0
sudosu18 <impoinfor.id@gmail.com>
2022-03-08 02:19:29 +0800

    Slider image V1.0

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const sideNav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sideNav);
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 600,
+    interval: 3000
+});
