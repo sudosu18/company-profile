b42d35548746cbef92003edca238e26a62a501e5
sudosu18 <impoinfor.id@gmail.com>
2022-03-09 01:36:53 +0800

    M2Collection V1.0

--- js/script.js
+++ js/script.js
@@ -11,3 +11,11 @@ M.Slider.init(slider, {
 
 const parallax = document.querySelectorAll('.parallax');
 M.Parallax.init(parallax);
+
+const materialboxed = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialboxed);
+
+const scrollspy = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scrollspy, {
+    scrollOffset : 50
+});
